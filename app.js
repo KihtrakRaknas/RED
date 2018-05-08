02628521b29ae76cb40b5ef26f003c9346029491
KihtrakRaknas <karthikandsankar@gmail.com>
2018-05-08 18:15:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var url = "www.google.com";
+var url = "http://www.google.com";
 document.onload = function(){
 document.getElementById("url").innerHTML = url;
 document.getElementById("url").href = url;
