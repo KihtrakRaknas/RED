446ed788ff9c0cbc7541d3ab9d783f916dbe8dad
KihtrakRaknas <karthikandsankar@gmail.com>
2018-05-09 09:49:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ console.log("TEST");
 var url = "http://bing.com";
 var urlEle = document.getElementById("url");
 var websiteUrlEle = document.getElementById("weburls");
+var failedUrl = document.getElementById("failedUrl");
 $(document).ready(function() {
+    if(url.substring(0,7)=="http://"||url.substring(0,8)=="https://")
     urlEle.innerHTML = url;
     urlEle.setAttribute("href", url);
     websiteUrlEle.setAttribute("href", url);
     websiteUrlEle.innerHTML = websiteUrlEle.hostname;
     console.log("TEST");
+    else{
+        failedUrl.innerHTML = url;
+    }
 });
 //window.location.replace(url);
