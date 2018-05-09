2c81dcd3b8b0cbdd669d17feadff13859f6ba82a
KihtrakRaknas <karthikandsankar@gmail.com>
2018-05-09 10:36:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-console.log("TEST");
+  var config = {
+    apiKey: "AIzaSyBpAWpSB1sSjuV1qR8woZoamGVonMgYN_Y",
+    authDomain: "red-irector.firebaseapp.com",
+    databaseURL: "https://red-irector.firebaseio.com",
+    projectId: "red-irector",
+    storageBucket: "red-irector.appspot.com",
+    messagingSenderId: "578371314492"
+  };
+  firebase.initializeApp(config);
+
 var url = "bing.com";
+
+firebase.database().ref(code).once('value').then(function(snapshot) {
+    url = snapshot.val().url;
+    console.log(url):
+}
+                                                 
 var urlEle = document.getElementById("url");
 var websiteUrlEle = document.getElementById("weburls");
 var failedUrl = document.getElementById("failedUrl");
@@ -17,3 +32,5 @@ $(document).ready(function() {
     }
 });
 window.location.replace(url);
+
+
