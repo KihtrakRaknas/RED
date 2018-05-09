dec3317f6c289af86e630466afc1c377081a4dc0
KihtrakRaknas <karthikandsankar@gmail.com>
2018-05-09 10:25:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var urlEle = document.getElementById("url");
 var websiteUrlEle = document.getElementById("weburls");
 var failedUrl = document.getElementById("failedUrl");
 $(document).ready(function() {
-    if(url.substring(0,7)=="http://"||url.substring(0,8)=="https://")){
+    if(url.substring(0,7)=="http://"||url.substring(0,8)=="https://"){
         urlEle.innerHTML = url;
         urlEle.setAttribute("href", url);
         websiteUrlEle.setAttribute("href", url);
