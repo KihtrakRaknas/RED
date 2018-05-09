2cc321ca00ecd94ac5a8410551181603e2a066d4
KihtrakRaknas <karthikandsankar@gmail.com>
2018-05-09 09:53:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var websiteUrlEle = document.getElementById("weburls");
 var failedUrl = document.getElementById("failedUrl");
 $(document).ready(function() {
     if(url.substring(0,7)=="http://"||url.substring(0,8)=="https://")
-    urlEle.innerHTML = url;
-    urlEle.setAttribute("href", url);
-    websiteUrlEle.setAttribute("href", url);
-    websiteUrlEle.innerHTML = websiteUrlEle.hostname;
-    console.log("TEST");
-    else{
+        urlEle.innerHTML = url;
+        urlEle.setAttribute("href", url);
+        websiteUrlEle.setAttribute("href", url);
+        websiteUrlEle.innerHTML = websiteUrlEle.hostname;
+        console.log("TEST");
+    }else{
+        noUrl.style.display = "block";
+        yesUrl.style.display = "none";
         failedUrl.innerHTML = url;
     }
 });
