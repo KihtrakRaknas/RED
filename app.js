df36101a9b0ba7a83a0f5a4afaf0b85719fab2b8
Kihtrak <karthikandsankar@gmail.com>
2018-05-10 18:36:36 -0400

    another quick fix

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var url = "bing.com";
 console.log(window.location.pathname);
-console.log(window.location.pathname.substring(window.location.pathname.lastIndexOf("\\")));
-firebase.database().ref(window.location.pathname.substring(window.location.pathname.lastIndexOf("//"))).once('value').then(function(snapshot) {
+console.log(window.location.pathname.substring(window.location.pathname.lastIndexOf("/")));
+firebase.database().ref(window.location.pathname.substring(window.location.pathname.lastIndexOf("/"))).once('value').then(function(snapshot) {
     url = snapshot.val().url;
     console.log(url);
 });
