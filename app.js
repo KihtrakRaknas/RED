215b449f0185a252326ba280f80e21498e840584
Kihtrak <karthikandsankar@gmail.com>
2018-05-10 21:58:19 -0400

    added index.html page

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
         firebase.initializeApp({apiKey: "AIzaSyBpAWpSB1sSjuV1qR8woZoamGVonMgYN_Y",authDomain: "red-irector.firebaseapp.com",databaseURL: "https://red-irector.firebaseio.com",projectId: "red-irector",storageBucket: "red-irector.appspot.com",messagingSenderId: "578371314492"});
-        console.log(window.location.pathname.substring(window.location.pathname.lastIndexOf("/")+1));
-try{ firebase.database().ref(window.location.pathname.substring(window.location.pathname.lastIndexOf("/")+1)+"/url").once('value').then(function(snapshot) {
-            try {
-                var testURL = new URL(snapshot.val());
-                window.location.replace(testURL);
-                $(document).ready(function() {
-                    var urlEle = document.getElementById("url");
-                    var websiteUrlEle = document.getElementById("weburls");
-                    urlEle.innerHTML = testURL;
-                    urlEle.setAttribute("href", testURL);
-                    websiteUrlEle.setAttribute("href", testURL);
-                    websiteUrlEle.innerHTML = websiteUrlEle.hostname;
-                });
-            }catch(error){
-                $(document).ready(function() {
-                    if(snapshot.val()==undefined){
-                        document.getElementById("noneUrl").style.display = "block";
-                        document.getElementById("yesUrl").style.display = "none";
-                    }else{
-                        document.getElementById("noUrl").style.display = "block";
-                        document.getElementById("yesUrl").style.display = "none";
-                        document.getElementById("failedUrl").innerHTML = snapshot.val();
-                    }
-                });
-            }
+
+var txtEmail = document.getElementById("txtEmail");
+var txtPassword = document.getElementById("txtPassword");
+var btnLogin = document.getElementById("btnLogin");
+var btnSignUp = document.getElementById("btnSignUp");
+var btnSignUpWithGoogle = document.getElementById("btnSignUpWithGoogle");
+
+btnLogin.addEventListener("click", ()=>{
+    console.log("TEST");
+    const email = txtEmail.value
+    const password = txtPassword.value
+    const promise = firebase.auth().signInWithEmailAndPassword(b, c);
+    promise.catch(pro=>{
+        console.log(pro.messsage),
+        $(document).ready(function() {
+            $("#logFailedAlert").slideDown().delay("100").slideUp()
+        })
+    });
+});
+
+var userID;
+btnSignUp.addEventListener("click", ()=>{
+    const email = txtEmail.value
+    const password = txtPassword.value
+    const promise = firebase.auth().createUserWithEmailAndPassword(b, c);
+    promise.catch(pro=>{
+        console.log(pro.messsage),
+        $(document).ready(function() {
+            $("#logFailedAlert").slideDown().delay("100").slideUp()
         });
-   }catch(error){
-    //var urlp = window.location.pathname.substring(window.location.pathname.lastIndexOf("/")+1);
-    //if(urlp.val().contains(".")||urlp.val().contains("#")||urlp.val().contains("$")||urlp.val().contains("[")||urlp.val().contains("]")){
-         window.onload = function() {
-            document.getElementById("noneUrl").style.display = "block";
-            document.getElementById("yesUrl").style.display = "none";
-            document.getElementById("register").innerHTML="invalid REDurl";
-            document.getElementById("register").classList.add("disabled");
-            document.getElementById("register").classList.remove("btn-success");
-            document.getElementById("register").classList.add("btn-danger");
-         }
-    //}
-   }
+    });
+});
+
+var provider = new firebase.auth.GoogleAuthProvider();
+btnSignUpWithGoogle.addEventListener("click", ()=>{
+    firebase.auth().signInWithRedirect(provider),
+    firebase.auth().getRedirectResult().then().catch(function(pro) {
+        console.log(pro),
+        $(document).ready(function() {
+            $("#logFailedAlert").slideDown().delay("100").slideUp()
+        })
+    })
+}
+);
