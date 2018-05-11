80070b1eac628204136ed02c27f14a67e71b6774
Kihtrak <karthikandsankar@gmail.com>
2018-05-10 20:33:10 -0400

    deleted old code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 firebase.initializeApp({apiKey: "AIzaSyBpAWpSB1sSjuV1qR8woZoamGVonMgYN_Y",authDomain: "red-irector.firebaseapp.com",databaseURL: "https://red-irector.firebaseio.com",projectId: "red-irector",storageBucket: "red-irector.appspot.com",messagingSenderId: "578371314492"});
 
-var url = "bing.com";
 console.log(window.location.pathname);
 console.log(window.location.pathname.substring(window.location.pathname.lastIndexOf("/")+1));
 firebase.database().ref(window.location.pathname.substring(window.location.pathname.lastIndexOf("/")+1)+"/url").once('value').then(function(snapshot) {
@@ -14,31 +13,17 @@ firebase.database().ref(window.location.pathname.substring(window.location.pathn
         websiteUrlEle.setAttribute("href", testURL);
         websiteUrlEle.innerHTML = websiteUrlEle.hostname;
     }catch(error){
-        var failedUrl = document.getElementById("failedUrl");
-        noUrl.style.display = "block";
-        yesUrl.style.display = "none";
-        failedUrl.innerHTML = snapshot.val();
+        if(snapshot.val()==undefined){
+            noneUrl.style.display = "block";
+            yesUrl.style.display = "none";
+        }else{
+            var failedUrl = document.getElementById("failedUrl");
+            noUrl.style.display = "block";
+            yesUrl.style.display = "none";
+            failedUrl.innerHTML = snapshot.val();
+        }
     };
     console.log(snapshot.val());
     console.log(snapshot.val().href);
     console.log(testURL);
 });
-                                                 
-
-
-/*$(document).ready(function() {
-    if(url.substring(0,7)=="http://"||url.substring(0,8)=="https://"){
-        urlEle.innerHTML = url;
-        urlEle.setAttribute("href", url);
-        websiteUrlEle.setAttribute("href", url);
-        websiteUrlEle.innerHTML = websiteUrlEle.hostname;
-        console.log("TEST");
-    }else{
-        noUrl.style.display = "block";
-        yesUrl.style.display = "none";
-        failedUrl.innerHTML = url;
-    }
-});*/
-
-
-
